c29ea159fa6466cc592572b6ee0d615e0c49838a
Rohit <a.gm>
2023-01-16 16:14:37 -0800

    te

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> testing js app.
+          Edit <code>src/App.js</code> testing.
         </p>
         <a
           className="App-link"
